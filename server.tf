8d3c7ae0d6d7aecb803d23925415054e07e45a4a
Gagandeep Singh <admin.gagan@gmail.com>
2021-06-30 12:58:06 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -4,10 +4,10 @@ region = "us-east-2"
 resource "aws_instance" "myawsserver" {
   ami = "ami-077e31c4939f6a2f3"
   instance_type = "t2.micro"
-  key_name = "ansible-master-server"
+  key_name = "gagan-import"
 
   tags = {
-    Name = "Techlanders-aws-ec2-instance"
+    Name = "Gagan-devops-ec2-instance"
     env = "test"
   }
   provisioner "local-exec" {
