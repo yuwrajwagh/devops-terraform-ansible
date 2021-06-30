b40003f71ed0ef86276bf79852fe71be779883b0
Gagandeep Singh <admin.gagan@gmail.com>
2021-06-30 13:10:21 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "myawsserver" {
     env = "test"
   }
   provisioner "local-exec" {
-    command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} >> /root/inv"
+    command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /root/inv"
   }
 }
 
