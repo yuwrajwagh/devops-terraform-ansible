961ddfa4f06516c584eacf80a0e562dda63ed965
Gagandeep Singh <admin.gagan@gmail.com>
2021-11-12 14:55:35 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -4,7 +4,7 @@ region = "us-east-2"
 resource "aws_instance" "myawsserver" {
   ami = "ami-077e31c4939f6a2f3"
   instance_type = "t2.micro"
-  key_name = "gagan-import"
+  key_name = "gds-import"
 
   tags = {
     Name = "Gagan-devops-ec2-instance-v1"
