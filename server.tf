6afa97c88d611999b2226e1f9dadd7ec1b5c742f
ramannkhanna2 <89792896+ramannkhanna2@users.noreply.github.com>
2022-06-09 21:12:00 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -4,12 +4,12 @@ region = "us-east-2"
 resource "aws_instance" "myawsserver" {
   ami = "ami-077e31c4939f6a2f3"
   instance_type = "t2.micro"
-  key_name = "gagan-import"
+  key_name = "raman-import"
 
   tags = {
-    Name = "Gagan-DevOps-Zensar2-batch2-server"
+    Name = "Raman-DevOps-Synechron-batch-server"
     env = "production"
-    owner = "Gagandeep"
+    owner = "Raman"
   }
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /tmp/inv"
